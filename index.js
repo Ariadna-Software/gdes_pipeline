025ad75ff513c623a9e25e791266170a35906aa0
Rafael Garcia <rafa@myariadna.com>
2018-06-06 18:12:23 +0200

    CAmbio T1TB1

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ router.get('/', function (req, res) {
 // -- registering routes
 app.use('/login', require('./lib/login/login_controller'));
 app.use('/version', require('./lib/version/version_controller'));
+app.use('/pwbi', require('./lib/pwbi/pwbi.controller'));
 app.use('/api', router);
 app.use('/api/estados', require('./lib/estados/estados.controller'));
 app.use('/api/grupos-usuarios', require('./lib/grupos-usuarios/grupos-usuarios.controller'));
--- public/js/app/infT1TB1.js
+++ public/js/app/infT1TB1.js
@@ -39,36 +39,48 @@ var apiInfT1TB1 = {
         dFecha = apiComunGeneral.gup("dFecha");
         hFecha = apiComunGeneral.gup("hFecha");
         // llamar al informe 
-        apiInfT1TB1.obtainReport();
+        apiInfT1TB1.obtainJSON();
     },
-    obtainReport: function () {
+    obtainJSON: function () {
+        var url = "/pwbi/T1TB1?fase=" + fase + "&pais=" + pais + "&dFecha=" + dFecha + "&hFecha=" + hFecha;
+        apiComunAjax.llamadaGeneral("GET", myconfig.apiUrl + url, data, function (err, data) {
+            if (err) return;
+            apiInfT1TB1.obtainReport(data);
+        });
+    },
+    obtainReport: function (data) {
         StiOptions.WebServer.url = "/streport";
         var file = "reports/T1TB1.mrt";
         // Create a new report instance
         var report = new Stimulsoft.Report.StiReport();
         report.loadFile(file);
 
-        var connectionString = "Server=" + myconfig.report.host + ";";
-        connectionString += "Port=" + myconfig.report.port + ";"
-        connectionString += "Database=" + myconfig.report.database + ";"
-        connectionString += "UserId=" + myconfig.report.user + ";"
-        connectionString += "Pwd=" + myconfig.report.password + ";";
-        report.dictionary.databases.list[0].connectionString = connectionString;
+        // var connectionString = "Server=" + myconfig.report.host + ";";
+        // connectionString += "Port=" + myconfig.report.port + ";"
+        // connectionString += "Database=" + myconfig.report.database + ";"
+        // connectionString += "UserId=" + myconfig.report.user + ";"
+        // connectionString += "Pwd=" + myconfig.report.password + ";";
+        // report.dictionary.databases.list[0].connectionString = connectionString;
 
-        var sql = report.dataSources.items[0].sqlCommand;
-        var sql2 = apiInfT1TB1.processParameters(sql);
-        report.dataSources.items[0].sqlCommand = sql2;
+        // var sql = report.dataSources.items[0].sqlCommand;
+        // var sql2 = apiInfT1TB1.processParameters(sql);
+        // report.dataSources.items[0].sqlCommand = sql2;
 
+        var dataSet = new Stimulsoft.System.Data.DataSet("T1TB1");
+        dataSet.readJson(JSON.stringify(data));
+        // Remove all connections from the report template
+        report.dictionary.databases.clear();
         // Parámetros
         report.dictionary.variables.items[0].val = dFecha;
         report.dictionary.variables.items[1].val = hFecha;
-
-
+        //
+        report.regData(dataSet.dataSetName, "", dataSet);
+        report.dictionary.synchronize();
         // Assign report to the viewer, the report will be built automatically after rendering the viewer
         viewer.report = report;
         viewer.renderHtml("report_viewer");
     },
-    processParameters: function(sql) {
+    processParameters: function (sql) {
         if (fase != "") sql += " AND o.faseOfertaId = " + fase;
         if (pais != "") sql += " AND o.paisId = " + pais;
         if (dFecha != "") {
